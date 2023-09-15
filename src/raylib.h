2c38cad55f52a96fa3cb7b801e858ee64e442a36
Neolisk <Dr.Neolisk@gmail.com>
2023-09-15 00:16:15 -0700

    Added Key, MouseButton and Char callbacks

--- src/raylib.h
+++ src/raylib.h
@@ -1095,6 +1095,18 @@ RLAPI char *EncodeDataBase64(const unsigned char *data, int dataSize, int *outpu
 RLAPI unsigned char *DecodeDataBase64(const unsigned char *data, int *outputSize);                    // Decode Base64 string data, memory must be MemFree()
 
 //------------------------------------------------------------------------------------
+// User Defined Input Callbacks (Module: core)
+//------------------------------------------------------------------------------------
+
+typedef void (*UserKeyCallback)(int, int, int, int);        // Type for key event callback
+typedef void (*UserMouseButtonCallback)(int, int, int);     // Type for mouse button event callback
+typedef void (*UserCharCallback)(unsigned int);             // Type for char event callback
+
+RLAPI void SetUserKeyCallback(UserKeyCallback callback);                     // Set user defined callback for key events
+RLAPI void SetUserMouseButtonCallback(UserMouseButtonCallback callback);     // Set user defined callback for mouse button events
+RLAPI void SetUserCharCallback(UserCharCallback callback);                   // Set user defined callback for char events
+
+//------------------------------------------------------------------------------------
 // Input Handling Functions (Module: core)
 //------------------------------------------------------------------------------------
 
--- src/rcore.c
+++ src/rcore.c
@@ -504,6 +504,11 @@ typedef struct CoreData {
 #endif
         unsigned int frameCounter;          // Frame counter
     } Time;
+    struct {
+        UserKeyCallback keyCallback;                 // User defined callback for GLFW key events
+        UserMouseButtonCallback mouseButtonCallback; // User defined callback for GLFW mouse button events
+        UserCharCallback charCallback;               // User defined callback for GLFW char events
+    } Callbacks;
 } CoreData;
 
 //----------------------------------------------------------------------------------
@@ -3588,6 +3593,28 @@ void OpenURL(const char *url)
         (*vm)->DetachCurrentThread(vm);
 #endif
     }
+}
+
+//----------------------------------------------------------------------------------
+// User Callbacks for GLFW events
+//----------------------------------------------------------------------------------
+
+// Set user defined callback for key events
+void SetUserKeyCallback(GLFWkeyfun callback)
+{
+    CORE.Callbacks.keyCallback = callback;
+}
+
+// Set user defined callback for mouse button events
+void SetUserMouseButtonCallback(GLFWmousebuttonfun callback)
+{
+    CORE.Callbacks.mouseButtonCallback = callback;
+}
+
+// Set user defined callback for char events
+void SetUserCharCallback(GLFWcharfun callback)
+{
+    CORE.Callbacks.charCallback = callback;
 }
 
 //----------------------------------------------------------------------------------
